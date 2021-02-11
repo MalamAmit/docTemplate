45c8c1696fc0c728c47aab4fd0aec9254913a3c9
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-11 14:16:06 +0200

    m

--- src/amit/doc/template/WebContent/DocTemplatePlugin.js
+++ src/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -1,26 +1,37 @@
-require(["dojo/_base/declare",
-        "dojo/_base/lang",
+
+require(["dojo/_base/declare","dojo/_base/lang",
         "ecm/widget/dialog/AddContentItemDialog"],
-    function (declare, lang, AddContentItemDialog) {
+    function(declare, lang, AddContentItemDialog) {
         /**
          * Use this function to add any global JavaScript methods your plug-in requires.
          */
         ``
         lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
-            console.log("amit - createDocFromTemplateAction");
 
-            // var templateFolderPath = repository.templateFolderPath;
-            var showListOfTemplates = function () {
-                console.log("amit - showListOfTemplates");
-            }
-            var param = "/test";
-            repository.retrieveItem(param, function (){
-                var addContentItemDialog = new AddContentItemDialog();
-            //     // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
-                addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
-                addContentItemDialog.set("title", "choose template");
-                addContentItemDialog.setIntroText("Na nan an nnnaa.");
-            })
+            //CM code: folder-PID of item for template folder structure
+            var _createFolderSubStructure = function (dossierFolder) {
+                console.log("Dossier folder created: " + dossierFolder.name);
+            };
+
+            repository.retrieveItem(dossierRootFolder,
+                function(dossierRootFolderItem) {
+                    var addContentItemDialog = new AddContentItemDialog();
+                    addContentItemDialog.setDefaultContentClass(dossierFolderClass);
+                    addContentItemDialog.show(repository, dossierRootFolderItem,
+                        false, false, _createFolderSubStructure, null, false, null);
+                    addContentItemDialog.set("title","Create new Dossier");
+                    addContentItemDialog.setIntroText("This folder will be the top level folder of your dossier.");
+                });
+
+
+            // var param = "/test";
+            // repository.retrieveItem(param, function (){
+            //     var addContentItemDialog = new AddContentItemDialog();
+            // //     // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
+            //     addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
+            //     addContentItemDialog.set("title", "choose template");
+            //     addContentItemDialog.setIntroText("Na nan an nnnaa.");
+            // })
 
 
 
