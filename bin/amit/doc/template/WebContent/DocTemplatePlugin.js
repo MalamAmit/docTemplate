5734c9322279035f75b805732e8b81bd0899dce4
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-11 10:24:56 +0200

    a

---
+++ bin/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -0,0 +1,19 @@
+require(["dojo/_base/declare",
+         "dojo/_base/lang"], 
+function(declare, lang) {		
+	/**
+	 * Use this function to add any global JavaScript methods your plug-in requires.
+	 */
+	
+	lang.setObject("createDocFromTemplateAction", function(repository, items, callback, teamspace, resultSet, parameterMap) {
+		debugger;
+		console.log("amit - createDocFromTemplateAction");
+
+		// var templateFolderPath = repository.templateFolderPath;
+
+
+		/*
+	  * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
+	  */
+	});
+});
---
+++ src/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -0,0 +1,27 @@
+require(["dojo/_base/declare",
+        "dojo/_base/lang"],
+    function (declare, lang, AddContentItemDialog) {
+        /**
+         * Use this function to add any global JavaScript methods your plug-in requires.
+         */
+        ``
+        lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
+            console.log("amit - createDocFromTemplateAction");
+
+            // var templateFolderPath = repository.templateFolderPath;
+            var showListOfTemplates = function () {
+                console.log("amit - showListOfTemplates");
+            }
+
+            var addContentItemDialog = new AddContentItemDialog();
+            // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
+            addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
+            addContentItemDialog.set("title", "choose template");
+            addContentItemDialog.setIntroText("Na nan an nnnaa.");
+
+
+            /*
+          * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
+          */
+        });
+    });
