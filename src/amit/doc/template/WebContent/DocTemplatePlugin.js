100e3124f3335d915a455ae756cf3e5cfeef9b3a
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-11 11:54:42 +0200

    a

--- src/amit/doc/template/WebContent/DocTemplatePlugin.js
+++ src/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -14,13 +14,13 @@ require(["dojo/_base/declare",
                 console.log("amit - showListOfTemplates");
             }
 
-            repository.retrieveItem(function (){
-                var addContentItemDialog = new AddContentItemDialog();
-                // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
-                addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
-                addContentItemDialog.set("title", "choose template");
-                addContentItemDialog.setIntroText("Na nan an nnnaa.");
-            })
+            // repository.retrieveItem(function (){
+            //     var addContentItemDialog = new AddContentItemDialog();
+            //     // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
+            //     addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
+            //     addContentItemDialog.set("title", "choose template");
+            //     addContentItemDialog.setIntroText("Na nan an nnnaa.");
+            // })
 
 
 
