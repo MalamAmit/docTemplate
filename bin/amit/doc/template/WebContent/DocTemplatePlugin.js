90e451852539eb9d7fba7d37e1a7132edbbb964e
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-14 12:09:20 +0200

    a

--- bin/amit/doc/template/WebContent/DocTemplatePlugin.js
+++ bin/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -1,19 +1,42 @@
-require(["dojo/_base/declare",
-         "dojo/_base/lang"], 
-function(declare, lang) {		
-	/**
-	 * Use this function to add any global JavaScript methods your plug-in requires.
-	 */
-	
-	lang.setObject("createDocFromTemplateAction", function(repository, items, callback, teamspace, resultSet, parameterMap) {
-		debugger;
-		console.log("amit - createDocFromTemplateAction");
 
-		// var templateFolderPath = repository.templateFolderPath;
+require(["dojo/_base/declare","dojo/_base/lang",
+        "ecm/widget/dialog/AddContentItemDialog"],
+    function(declare, lang ) {
+        lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
+            var dossierRootFolder = "/CustomerDossiers"; //CM code:folder-PID
+            var dossierFolderClass = "CustomerDossier"; //CM code: item type
 
 
-		/*
-	  * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
-	  */
-	});
-});
+            //CM code: folder-PID of item for template folder structure
+            var func = function () {
+                console.log("open dialog");
+            };
+
+            repository.retrieveItem(dossierRootFolder,
+                function(dossierRootFolderItem) {
+                    var addContentItemDialog = new AddContentItemDialog();
+
+                    // addContentItemDialog.setDefaultContentClass(class...);
+
+                    addContentItemDialog.show(repository, dossierRootFolderItem, false, false, func, null, false, null);
+                    addContentItemDialog.set("title","Show templates for doc");
+                    addContentItemDialog.setIntroText("descriptions...");
+                });
+
+
+            // var param = "/test";
+            // repository.retrieveItem(param, function (){
+            //     var addContentItemDialog = new AddContentItemDialog();
+            // //     // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
+            //     addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
+            //     addContentItemDialog.set("title", "choose template");
+            //     addContentItemDialog.setIntroText("Na nan an nnnaa.");
+            // })
+
+
+
+            /*
+          * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
+          */
+        });
+    });
--- src/amit/doc/template/WebContent/DocTemplatePlugin.js
+++ src/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -1,42 +1,15 @@
-
-require(["dojo/_base/declare","dojo/_base/lang",
+require(["dojo/_base/declare", "dojo/_base/lang",
         "ecm/widget/dialog/AddContentItemDialog"],
-    function(declare, lang, ) {
+    function (declare, lang) {
         lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
-            var dossierRootFolder = "/CustomerDossiers"; //CM code:folder-PID
-            var dossierFolderClass = "CustomerDossier"; //CM code: item type
 
+            Request.invokePluginService("DocTemplatePlugin", "GetConfigurationService", {
+                requestCompleteCallback: function (response) {
 
-            //CM code: folder-PID of item for template folder structure
-            var func = function () {
-                console.log("open dialog");
-            };
-
-            repository.retrieveItem(dossierRootFolder,
-                function(dossierRootFolderItem) {
-                    var addContentItemDialog = new AddContentItemDialog();
-
-                    // addContentItemDialog.setDefaultContentClass(class...);
-
-                    addContentItemDialog.show(repository, dossierRootFolderItem, false, false, func, null, false, null);
-                    addContentItemDialog.set("title","Show templates for doc");
-                    addContentItemDialog.setIntroText("descriptions...");
-                });
-
-
-            // var param = "/test";
-            // repository.retrieveItem(param, function (){
-            //     var addContentItemDialog = new AddContentItemDialog();
-            // //     // addContentItemDialog.setDefaultContentClass(dossierFolderClass);
-            //     addContentItemDialog.show(repository, false, false, showListOfTemplates, null, false, null);
-            //     addContentItemDialog.set("title", "choose template");
-            //     addContentItemDialog.setIntroText("Na nan an nnnaa.");
-            // })
-
-
-
-            /*
-          * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
-          */
+                    console.log("amit - requestCompleteCallback")
+                    // var dossierRootFolder = response.configuration[1].value;
+                    // var dossierFolderClass = response.configuration[0].value;
+                } //requestCompleteCallback
+            });
         });
     });
