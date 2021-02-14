0f3a31783f991f1effce38e714b0abb4585d7ded
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-14 12:46:39 +0200

    a

--- src/amit/doc/template/WebContent/DocTemplatePlugin.js
+++ src/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -3,13 +3,13 @@ require(["dojo/_base/declare", "dojo/_base/lang",
     function (declare, lang) {
         lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
             console.log("amit 1 - requestCompleteCallback")
-            Request.invokePluginService("DocTemplatePlugin", "GetConfigurationService", {
-                requestCompleteCallback: function (response) {
-
-                    console.log("amit 2 - requestCompleteCallback")
-                    // var dossierRootFolder = response.configuration[1].value;
-                    // var dossierFolderClass = response.configuration[0].value;
-                } //requestCompleteCallback
-            });
+            // Request.invokePluginService("DocTemplatePlugin", "GetConfigurationService", {
+            //     requestCompleteCallback: function (response) {
+            //
+            //         console.log("amit 2 - requestCompleteCallback")
+            //         // var dossierRootFolder = response.configuration[1].value;
+            //         // var dossierFolderClass = response.configuration[0].value;
+            //     } //requestCompleteCallback
+            // });
         });
     });
