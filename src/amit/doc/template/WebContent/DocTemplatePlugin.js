5f7b74c12f48849406088c0150550126537d09b9
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-14 13:30:07 +0200

    a

--- src/amit/doc/template/WebContent/DocTemplatePlugin.js
+++ src/amit/doc/template/WebContent/DocTemplatePlugin.js
@@ -1,10 +1,8 @@
 require(["dojo/_base/declare",
         "dojo/_base/lang",
-        "ecm/widget/dialog/AddContentItemDialog",
-        "ecm/model/Request"],
-    function (declare, lang) {
-        lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap,
-                                                                Request) {
+        "ecm/model/Request",],
+    function (declare, lang, Request) {
+        lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
             console.log("amit 1 - requestCompleteCallback")
 
 
