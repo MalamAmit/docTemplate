5f9eef6db40d8a5f2645ddd3703702f7c7bdae2f
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-14 11:14:57 +0200

    a

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
@@ -1,22 +1,27 @@
-define([
-		"dojo/_base/declare",
-		"dijit/_TemplatedMixin",
-		"dijit/_WidgetsInTemplateMixin",
-		"ecm/widget/admin/PluginConfigurationPane",
-		"dojo/text!./templates/ConfigurationPane.html"
-	],
-	function(declare, _TemplatedMixin, _WidgetsInTemplateMixin, PluginConfigurationPane, template) {
+define(["dojo/_base/declare",
+        "dijit/_TemplatedMixin",
+        "dijit/_WidgetsInTemplateMixin",
+        "ecm/widget/ValidationTextBox",
+        "ecm/widget/admin/PluginConfigurationPane",
+        "dojo/text!./templates/ConfigurationPane.html"],
+    function (declare, _TemplatedMixin, _WidgetsInTemplateMixin, ValidationTextBox, PluginConfigurationPane, template) {
+        return declare("dossierPluginDojo.ConfigurationPane",
+            [PluginConfigurationPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
+                templateString: template,
+                widgetsInTemplate: true,
 
-		return declare("DocTemplatePluginDojo.ConfigurationPane", [ PluginConfigurationPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
-		
-		templateString: template,
-		widgetsInTemplate: true,
-	
-		load: function(callback) {
-		},
-		
-		validate: function() {
-			return true;
-		}
-	});
-});
+                load: function (callback) {
+                    if (this.configurationString) {
+                        var jsonConfig = JSON.parse(this.configurationString);
+                        this.enableFolderClassName.set('value', jsonConfig.configuration[0].value);
+                        this.folderTemplateName.set('value', jsonConfig.configuration[1].value);
+                        this.templateFolderStructureField.set('value',
+                            jsonConfig.configuration[2].value);
+                    }
+                },
+
+                validate: function () {
+                    return true;
+                }
+            });
+    });
