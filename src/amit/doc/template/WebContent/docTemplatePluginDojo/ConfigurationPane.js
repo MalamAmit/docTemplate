77a160d5c079298cd1a6dccf4faf540056f346dc
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-16 15:52:00 +0200

    a

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
@@ -21,17 +21,17 @@ define(["dojo/_base/declare",
                     this.enableFolderClassName.setRootClassId("Folder");
 
 
-                    // if (this.configurationString) {
-                    //     var jsonConfig = JSON.parse(this.configurationString);
+                    if (this.configurationString) {
+                        var jsonConfig = JSON.parse(this.configurationString);
                     //
-                    //     if (jsonConfig.configuration[0].value !== undefined) {
-                    //         this.enableFolderClassName.setSelected(jsonConfig.configuration[0].value);
+                        if (jsonConfig.configuration[0].value !== undefined) {
+                            this.enableFolderClassName.setSelected(jsonConfig.configuration[0].value);
                     //         // this.enableFolderClassName.setSelected("Folder");
                     //         // } else {
                     //         // this.enableFolderClassName.setRootClassId(jsonConfig.configuration[0].value);
-                    //     }
+                        }
                     //     this.folderTemplateName.set('value', jsonConfig.configuration[1].value);
-                    // }
+                    }
 
                 },
 
