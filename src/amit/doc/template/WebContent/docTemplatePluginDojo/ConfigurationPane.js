c4b58aed3b9d8887b1c46e2a701a56c1d20be283
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-16 15:19:52 +0200

    a

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
@@ -18,62 +18,23 @@ define(["dojo/_base/declare",
                     this.repository = ecm.model.desktop.getRepositoryByName("OS1");
                     this.enableFolderClassName.setRepository(this.repository);
                     this.enableFolderClassName.setVisibleOnlyForFolder(true);
+                    this.enableFolderClassName.setRootClassId("Folder");
 
 
                     if (this.configurationString) {
                         var jsonConfig = JSON.parse(this.configurationString);
-                        // this.enableFolderClassName.set('value', jsonConfig.configuration[0].value);
 
-
-                        console.log("jsonConfig.configuration[0] " + jsonConfig.configuration[0])
-                        console.log("jsonConfig.configuration[0].value " + jsonConfig.configuration[0].value)
-                        console.log("jsonConfig.configuration[0].value === undefined " + jsonConfig.configuration[0].value === undefined)
-                        if (jsonConfig.configuration[0].value === undefined) {
-                            console.log("4")
-                            this.enableFolderClassName.setRootClassId("Folder");
-                        } else {
-                            this.enableFolderClassName.setRootClassId(jsonConfig.configuration[0].value);
-
+                        if (jsonConfig.configuration[0].value !== undefined) {
+                            this.enableFolderClassName.setSelected(jsonConfig.configuration[0].value);
+                            // this.enableFolderClassName.setSelected("Folder");
+                        // } else {
+                        //     this.enableFolderClassName.setRootClassId(jsonConfig.configuration[0].value);
                         }
-
                         this.folderTemplateName.set('value', jsonConfig.configuration[1].value);
-                    } else {
-                        this.enableFolderClassName.setRootClassId("Folder");
-
-                        // var cb = lang.hitch(this, function (entryTemplates, cn1, cn2) {
-                        //     var items = [];
-                        //
-                        //     array.forEach(entryTemplates, function (ent) {
-                        //         items.push({
-                        //             id: ent.vsId,
-                        //             name: ent.name,
-                        //             className: ent.addClassName,
-                        //             vsId: ent.vsId,
-                        //             entId: ent.id
-                        //         })
-                        //     }, this);
-                        //
-                        // });
                     }
                 },
 
                 _onParamChange: function () {
-                //     var configArray = new Array();
-                //     var configString = {
-                //         name: "enableFolderClassName",
-                //         value: this.enableFolderClassName.get('value')
-                //     };
-                //     configArray.push(configString);
-                //     configString = {
-                //         name: "folderTemplateName",
-                //         value: this.folderTemplateName.get('value')
-                //     };
-                //     configArray.push(configString);
-                //     var configJson = {
-                //         "configuration": configArray
-                //     };
-                //     this.configurationString = JSON.stringify(configJson);
-                //     console.log("configurationString 1" + this.configurationString)
                     this.onSaveNeeded(true);
                 },
 
