577b8f079534b2195454a7cbd40f1835af7fdcea
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-21 10:42:38 +0200

    m

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/RightClickDocFromTemplateAction.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/RightClickDocFromTemplateAction.js
@@ -17,15 +17,18 @@ define(["dojo/_base/declare", "ecm/model/Action"],
 
 
             isVisible: function (repository, listType) {
+                debugger;
                 return this.inherited(arguments);
             },
 
 
             performAction: function (repository, itemList, callback, teamspace, resultSet, parameterMap) {
+                debugger;
             },
 
 
             setBrowseRootFolder: function (newRootFolder, browseFeature) {
+                debugger;
                 browseFeature.folderTree.setFolder(newRootFolder);
                 // optionally set content list to the first child.
             }
