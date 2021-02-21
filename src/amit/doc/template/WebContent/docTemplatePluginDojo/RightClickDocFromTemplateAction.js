787b4750b66f775617291ffd0d9a8a5d34df3475
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-21 14:29:52 +0200

    m

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/RightClickDocFromTemplateAction.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/RightClickDocFromTemplateAction.js
@@ -1,18 +1,22 @@
 define(["dojo/_base/declare", "ecm/model/Action"],
-    function (declare, Action) {        return declare("docTemplatePluginDojo.RightClickDocFromTemplateAction", [Action], {
-
+    function (declare, Action) {
+        return declare("docTemplatePluginDojo.RightClickDocFromTemplateAction", [Action], {
 
 
             isEnabled: function (repository, listType, items, teamspace, resultSet) {
+
                 var enabled = this.inherited(arguments);
-                if (items && items[0].isFolder && items[0].getContentClass && !items[0]._folderContents) {
-                    // var sameClass = items[0].getContentClass().name == "CustomerDossier";
-                    // return enabled && items[0].isFolder() && sameClass;
-                    return true;
+                if (items && items[0].isFolder && items[0].getContentClass) {
+                    var hasChildren = !!items[0]._folderContents;
+                    if (hasChildren && !items[0]._folderContents.items) {
+                        hasChildren = false;
+                    }
+
+                    var sameClass = items[0].getContentClass().name == "CustomerDossier";
+                    return enabled && items[0].isFolder() && sameClass & !hasChildren;
                 }
                 return false;
             },
-
 
 
             isVisible: function (repository, listType) {
