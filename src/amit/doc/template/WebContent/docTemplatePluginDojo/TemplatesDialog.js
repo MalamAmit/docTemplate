694cbec79b9837d102392d541fad93b251e8f063
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-22 15:42:13 +0200

    m

---
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/TemplatesDialog.js
@@ -0,0 +1,12 @@
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "ecm/widget/dialog/BaseDialog",
+    "dojo/text!./templates/TemplatesDialog.html"
+], function (declare, lang, BaseDialog) {
+
+
+    return declare("docTemplatePluginDojo.templatesDialog", [BaseDialog], {
+
+    });
+});
