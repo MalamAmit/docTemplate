3e126a1ce919071d78803d1b1115393ebd1206a0
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-22 16:56:31 +0200

    m

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/TemplatesDialog.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/TemplatesDialog.js
@@ -6,8 +6,9 @@ define([
     "dijit/layout/ContentPane",
     "gridx/Grid",
     "ecm/widget/listView/gridModules/Async",
-    "gridx/modules/select/Row"
-], function (declare, lang, BaseDialog, template, ContentPane, Grid, Cache, SelectRow) {
+    "gridx/modules/select/Row",
+    "dojo/store/Memory"
+], function (declare, lang, BaseDialog, template, ContentPane, Grid, Cache, SelectRow, Memory) {
 
 
     return declare("docTemplatePluginDojo.templatesDialog", [BaseDialog], {
@@ -16,83 +17,69 @@ define([
         },
 
         _createGrid: function () {
-            if (this._resultSet != null) {
-                var structure = this._getGridStructure();
-                this._setEditorArguments(structure);
 
-                // Clear grid connections and destroy grid
-                if (this.grid) {
-                    this._clearGridConnections();
-                    this.grid.destroyRecursive();
-                }
-
-                if (this._detailsView)
-                    this._addDetailsViewColumnDecorators();
-                if (this._magazineView)
-                    this._addMagazineViewColumnDecorators();
-
-                // Destroying the grid does not free the store; must take
-                // care of that manually.
-                if (this.store)
-                    this.store = null;
-                // this.store = this._resultSet.getStore();
+            // Destroying the grid does not free the store; must take
+            // care of that manually.
+            if (this.store)
+                this.store = null;
+            // this.store = this._resultSet.getStore();
 
 
-                this.store = new Memory({
-                    data : [ {
-                        id : 1,
-                        column_1 : 'Feed4',
-                        column_2 : '444',
-                        column_3 : '',
-                        column_4 : ''
-                    }, {
-                        id: 2,
-                        column_1 : 'Feed5',
-                        column_2 : '555',
-                        column_3 : 0,
-                        column_4 : '',
-                    } ]})
-                debugger;
-                this.grid = new Grid({
-                    cacheClass: Cache,
-                    // pageSize: this._resultSet.pageSize,
-                    store: this.store,
-                    structure: [
-                        {id: "column_1", field: "name", name: "Name", width: "50%"},
-                        {id: "column_2", field: "genre", name: "Genre"},
-                        {id: "column_3", field: "composer", name: "Composer"},
-                        {id: "column_4", field: "year", name: "Year"}
-                    ],
-                    // selectRowTriggerOnCell: !has("ecmMobile") && !this._allowCheckboxes,
-                    modules: [SelectRow],
-                    // textDir: has("text-direction"),
-                    // headerHidden: this.headerHidden,
-                    contentList: this,
-                    selectRowTriggerOnCell: true,
-                    // barBottom: this._isShowPaginationBar() ? [ListViewPaginationBar] : null
+            this.store = new Memory({
+                data: [{
+                    id: 1,
+                    column_1: 'Feed4',
+                    column_2: '444',
+                    column_3: '',
+                    column_4: ''
+                }, {
+                    id: 2,
+                    column_1: 'Feed5',
+                    column_2: '555',
+                    column_3: 0,
+                    column_4: '',
+                }]
+            })
+            debugger;
+            this.grid = new Grid({
+                cacheClass: Cache,
+                // pageSize: this._resultSet.pageSize,
+                store: this.store,
+                structure: [
+                    {id: "column_1", field: "name", name: "Name", width: "50%"},
+                    {id: "column_2", field: "genre", name: "Genre"},
+                    {id: "column_3", field: "composer", name: "Composer"},
+                    {id: "column_4", field: "year", name: "Year"}
+                ],
+                // selectRowTriggerOnCell: !has("ecmMobile") && !this._allowCheckboxes,
+                modules: [SelectRow],
+                // textDir: has("text-direction"),
+                // headerHidden: this.headerHidden,
+                contentList: this,
+                selectRowTriggerOnCell: true,
+                // barBottom: this._isShowPaginationBar() ? [ListViewPaginationBar] : null
 
-                });
-                this.grid.placeAt(this.gridPoint);
-                this.grid.startup();
-                // // In IE, setting the vScrollerBuffSize helps with the focus on rows jumping back several rows when using the keyboard
-                // // to go up near the top of the list.
-                // if (!this._isShowPaginationBar() && (has("ie") || has("trident")) ) {
-                //     if (this.grid.rowCount() > 0) {
-                //         this.grid.vScrollerBuffSize = 0;		// row count nodes above/below the grid body viewport
-                //     }
-                // }
+            });
+            this.grid.placeAt(this.gridPoint);
+            this.grid.startup();
+            // // In IE, setting the vScrollerBuffSize helps with the focus on rows jumping back several rows when using the keyboard
+            // // to go up near the top of the list.
+            // if (!this._isShowPaginationBar() && (has("ie") || has("trident")) ) {
+            //     if (this.grid.rowCount() > 0) {
+            //         this.grid.vScrollerBuffSize = 0;		// row count nodes above/below the grid body viewport
+            //     }
+            // }
 
-                // this.grid.body._loadFail = lang.hitch(this, function(e) {
-                // });
-                // this.grid.contentList = this;
-                //
-                // this._grid = this.grid;
-                // this._createGridConnections();
-                //
-                // domConstruct.place(this.grid.domNode, this.gridArea.domNode, "only");
-                // this.grid.startup();
+            // this.grid.body._loadFail = lang.hitch(this, function(e) {
+            // });
+            // this.grid.contentList = this;
+            //
+            // this._grid = this.grid;
+            // this._createGridConnections();
+            //
+            // domConstruct.place(this.grid.domNode, this.gridArea.domNode, "only");
+            // this.grid.startup();
 
-            }
         },
     });
 });
