889bfbf09a69c97847b5e3f40cecabd38f66b11a
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-25 14:05:46 +0200

    m

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/RightClickDocFromTemplateAction.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/RightClickDocFromTemplateAction.js
@@ -45,77 +45,39 @@ define(["dojo/_base/declare",
 
 
             performAction: function (repository, itemList, callback, teamspace, resultSet, parameterMap) {
-                // var baseD = new BaseDialog();
-                // baseD.show();
-                // baseD.destroy();
 
 
-                var template = new SearchTemplate({
-                    id: "StoredSearch,{0DC081DE-3B0D-42C6-B213-63729230F9A9},{60F57A6E-0000-CD1D-815E-3F792C408580}",
-                    name: "StoredSearch",
-                    repository: ecm.model.desktop.getRepository("OS1"),
-                    description: "item.description"
-                });
+                var className = "";
+                var folderParams = {};
+                var self = this;
+                Request.invokePluginService("DocTemplatePlugin",
+                    "GetConfigurationService", {
+                        requestCompleteCallback: function (response) {
+                            className = response.enableFolderClassName;
+                            folderParams = response.folderSelectorParam;
 
-                self.search = new SearchDialog({
-                    searchTemplate: template,
-                    repository: ecm.model.desktop.getRepository("OS1"),
-                    showSearch: true,
-                    style: {minHeight: "700px", minWidth: "1000px"}
-                });
+                            //todo: replace id from configuration
+                            var template = new SearchTemplate({
+                                id: "StoredSearch,{0DC081DE-3B0D-42C6-B213-63729230F9A9},{60F57A6E-0000-CD1D-815E-3F792C408580}",
+                                name: "StoredSearch",
+                                repository: ecm.model.desktop.getRepository("OS1"),
+                                description: "item.description"
+                            });
 
-                self.search.setTitle("Choose template");
-                self.search.setMaximized(false)
-                self.search.addButton("Select Template", self.selectTemplate, false, true);
-                self.search.show();
-                self.search.destroy();
+                            self.search = new SearchDialog({
+                                searchTemplate: template,
+                                repository: ecm.model.desktop.getRepository("OS1"),
+                                showSearch: true,
+                                style: {minHeight: "700px", minWidth: "1000px"}
+                            });
 
-                // var className = "";
-                // var folderParams = {};
-                // var self = this;
-                // Request.invokePluginService("DocTemplatePlugin",
-                //     "GetConfigurationService", {
-                //         requestCompleteCallback: function (response) {
-                //             className = response.enableFolderClassName;
-                //             folderParams = response.folderSelectorParam;
-                //
-                //             var test = function (dossierFolder) {
-                //
-                //             }
-                //             // repository.retrieveItem(
-                //             //     "/CustomerDossiers",
-                //             //     function (rootFolder) {
-                //             //         var addContentItemDialog = new AddContentItemDialog();
-                //             //         addContentItemDialog.setDefaultContentClass(className);
-                //             //         addContentItemDialog.show(repository, rootFolder, false, false, test, null, false, null);
-                //             //         addContentItemDialog.set("title", "Create new Doc");
-                //             //         addContentItemDialog.setIntroText("Create new doc from template");
-                //             //     });
-                //             // var templatesDialog = new TemplatesDialog();
-                //             // templatesDialog.show()
-                //             // var cls = item._unifiedSearch ? UnifiedSearchTemplate : SearchTemplate;
-                //
-                //             //todo: replace id from configuration
-                //             var template = new SearchTemplate({
-                //                 id: "StoredSearch,{0DC081DE-3B0D-42C6-B213-63729230F9A9},{60F57A6E-0000-CD1D-815E-3F792C408580}",
-                //                 name: "StoredSearch",
-                //                 repository: ecm.model.desktop.getRepository("OS1"),
-                //                 description: "item.description"
-                //             });
-                //
-                //             self.search = new SearchDialog({
-                //                 searchTemplate: template,
-                //                 repository: ecm.model.desktop.getRepository("OS1"),
-                //                 showSearch: true,
-                //                 style: {minHeight: "700px", minWidth: "1000px"}
-                //             });
-                //
-                //             self.search.setTitle("Choose template");
-                //             self.search.setMaximized(false)
-                //             self.search.addButton("Select Template", self.selectTemplate, false, true);
-                //             self.search.show();
-                //         }
-                //     });
+                            self.search.setTitle("Choose template");
+                            self.search.setMaximized(false)
+                            self.search.addButton("Select Template", self.selectTemplate, false, true);
+                            self.search.show();
+                            self.search.destroy();
+                        }
+                    });
             },
 
             selectTemplate: function () {
