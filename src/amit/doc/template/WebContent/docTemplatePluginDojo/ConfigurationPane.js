bc9eb3e0bbd77a132e125525a79a9e01911644ac
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-02-28 15:19:25 +0200

    m

--- src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
+++ src/amit/doc/template/WebContent/docTemplatePluginDojo/ConfigurationPane.js
@@ -1,5 +1,11 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
+        "dojo/store/Memory",
+
+        "gridx/Grid",
+        "gridx/core/model/cache/Sync",
+        "gridx/modules/select/Row",
+
         "dijit/_TemplatedMixin",
         "dijit/_WidgetsInTemplateMixin",
         "ecm/widget/ValidationTextBox",
@@ -7,13 +13,85 @@ define(["dojo/_base/declare",
         "ecm/widget/_FolderSelectorDropDown",
         "ecm/widget/ContentClassSelector",
         "dojo/text!./templates/ConfigurationPane.html"],
-    function (declare, lang, _TemplatedMixin,
-              _WidgetsInTemplateMixin, ValidationTextBox,
-              PluginConfigurationPane, _FolderSelectorDropDown, ContentClassSelector, template) {
+    function (declare, lang, MemoryStore,
+              Grid, Cache, SelectRow,
+
+              _TemplatedMixin,
+              _WidgetsInTemplateMixin,
+              ValidationTextBox,
+              PluginConfigurationPane,
+              _FolderSelectorDropDown,
+              ContentClassSelector,
+              template) {
+
         return declare("docTemplatePluginDojo.ConfigurationPane",
             [PluginConfigurationPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
                 templateString: template,
                 widgetsInTemplate: true,
+
+                postCreate: function () {
+                    this.inherited(arguments);
+                    this.configurationGrid = this.createGrid();
+                },
+
+                createGrid: function () {
+
+                    var store = new MemoryStore({
+                        idProperty: "id",
+                        data: []
+                    });
+
+                    var structure = [
+                        {
+                            id: "repositoryId",
+                            field: "repositoryId",
+                            name: "Repository",
+                            width: "10%"
+                        },
+                        {
+                            id: "OrgUnit",
+                            field: "OrgUnit",
+                            name: "OrgUnit",
+                            width: "10%"
+                        },
+                        {
+                            id: "FolderClass ",
+                            field: "FolderClass ",
+                            name: "FolderClass ",
+                            width: "20%"
+                        },
+                        {
+                            id: "SearchTemplateVsId",
+                            field: "SearchTemplateVsId",
+                            name: "Search template VsId",
+                            width: "40%"
+                        },
+                    ];
+
+
+                    var grid = new Grid({
+                        cacheClass: Cache,
+                        store: store,
+                        structure: structure,
+                        autoHeight: true,
+                        modules: [
+                            SelectRow
+                        ],
+                        selectRowTriggerOnCell: true
+                    });
+
+                    grid.placeAt(this.myGrid);
+                    grid.startup();
+                    grid.resize();
+
+                    this.own(aspect.after(grid.select.row, "onSelectionChange", lang.hitch(this, function (evt) {
+                        this.checkFldAssButtons();
+                    }), true));
+
+
+                    return grid;
+                },
+
 
                 load: function (callback) {
                     this.repository = ecm.model.desktop.getRepositoryByName("OS1");
@@ -30,7 +108,7 @@ define(["dojo/_base/declare",
                         }
 
                         if (jsonConfig.folderSelectorParam !== undefined) {
-                            this.repository.retrieveItem(jsonConfig.folderSelectorParam.id, lang.hitch(this, function(item) {
+                            this.repository.retrieveItem(jsonConfig.folderSelectorParam.id, lang.hitch(this, function (item) {
                                 this.folderSelector.setSelected(item);
                             }));
                         }
@@ -45,8 +123,8 @@ define(["dojo/_base/declare",
                     var configJson = {};
                     configJson.enableFolderClassName = this.enableFolderClassName.getSelected().id;
                     configJson.folderSelectorParam = {
-                        id : this.folderSelector.getSelected().item.id,
-                        path : this.folderSelector.getSelected().path
+                        id: this.folderSelector.getSelected().item.id,
+                        path: this.folderSelector.getSelected().path
                     }
                     this.configurationString = JSON.stringify(configJson);
                 },
