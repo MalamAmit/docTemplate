eae01ddeaa37ed896224931ae4d4765edc2cad07
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-03-09 16:02:06 +0200

    m

--- src/com/malam/ecm/WebContent/newDocByTemplateDojo/SearchDialog.js
+++ src/com/malam/ecm/WebContent/newDocByTemplateDojo/SearchDialog.js
@@ -80,6 +80,7 @@ define([
                     this.setTitle(this.messages.workspacebuilder_search_dialog_title);
                 }
 
+                this.search.hideSearchCriteria = true;
                 this.search.repository = this.repository;
                 this.search.setSearchTemplate(this.searchTemplate);
 
