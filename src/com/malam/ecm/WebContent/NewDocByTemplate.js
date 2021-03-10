a45c0bd44a0aab92288ec006910c24fa2b655b27
Amit Yazdan <amit_y@jer.ad.malam.com>
2021-03-10 12:18:46 +0200

    olga

--- src/com/malam/ecm/WebContent/NewDocByTemplate.js
+++ src/com/malam/ecm/WebContent/NewDocByTemplate.js
@@ -2,12 +2,8 @@ require(["dojo/_base/declare",
         "dojo/_base/lang",
         "ecm/model/Request"],
     function (declare, lang, Request) {
-
-        lang.setObject("createDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
-        });
-
         lang.setObject("rightClickDocFromTemplateAction", function (repository, items, callback, teamspace, resultSet, parameterMap) {
-            console.log("rightClickDocFromTemplateAction")
+           // console.log("rightClickDocFromTemplateAction")
             /*
           * Add custom code for your action here. For example, your action might launch a dialog or call a plug-in service.
           */
--- src/com/malam/ecm/WebContent/newDocByTemplateDojo/action/RightClickDocFromTemplateAction.js
+++ src/com/malam/ecm/WebContent/newDocByTemplateDojo/action/RightClickDocFromTemplateAction.js
@@ -4,7 +4,7 @@ define(["dojo/_base/declare",
 
         "ecm/model/Action",
         "ecm/model/Request",
-        "ecm/widget/search/SearchDialog",
+        "newDocByTemplateDojo/Override/AmitSearchDialog",
         "ecm/model/SearchTemplate",
         "ecm/widget/dialog/AddDocumentFromEditServiceTemplateDialog",
         "ecm/widget/dialog/AddContentItemDialog",
@@ -13,53 +13,77 @@ define(["dojo/_base/declare",
 
         "newDocByTemplateDojo/LocalDefinition"],
     function (declare, lang, ExtMessages,
-              Action, Request, SearchDialog, SearchTemplate, AddDocumentFromEditServiceTemplateDialog, AddContentItemDialog, Desktop, CommonActionsHandler,
+              Action, Request, SearchDialog, SearchTemplate, AddDocumentFromEditServiceTemplateDialog, AddContentItemDialog, Desktop, CommonActionsHandler,array,
               LocalDefinition) {
         return declare("newDocByTemplateDojo.action.RightClickDocFromTemplateAction", [
             CommonActionsHandler,
             Action
         ], {
 
-            folderClassName: null,
+        	associateEntryTemplate: null,
             _extMessages: ExtMessages,
 
-            isEnabled: function (repository, listType, items, teamspace,
-                                 resultSet) {
+            isEnabled: function (repository, listType, items, teamspace,   resultSet) {
+            	debugger;
                 var enabled = this.inherited(arguments);
                 if (items && items[0].isFolder && items[0].getContentClass) {
-                    if (!this.folderClassName) {
-                        this.folderClassName = LocalDefinition.getCacheResponce().configurationGridData[0].folderClass;
-                    }
-                    var sameClass = (items[0].getContentClass().name == this.folderClassName);
-                    return enabled && items[0].isFolder() && sameClass;
+                	var currentFolder =items[0];
+    				//debugger;
+    			    this.associateEntryTemplate = array.filter(LocalDefinition.getCacheResponce().configurationGridData, function(fld) {
+    							if (currentFolder.attributes.FldNumerator && currentFolder.attributes.FldNumerator!=null)
+    								return fld.folderClass == currentFolder.getContentClass().id && currentFolder.attributes.FldNumerator.startsWith(fld.orgUnit)
+    							else
+    								return false;
+    						},this);
+    			 // debugger;
+    				return enabled && this.associateEntryTemplate && this.associateEntryTemplate.length == 1;
                 }
+             //debugger;
                 return false;
-            },
-
-            isGlobalEnabled: function (resultSet, items, repository) {
-                debugger;
-                var enabled = this.inherited(arguments);
-                // if (items && items[0].isFolder && items[0].getContentClass) {
-                //     if (!this.folderClassName) {
-                //         this.folderClassName = LocalDefinition.getCacheResponce().configurationGridData[0].folderClass;
-                //     }
-                //     var sameClass = (items[0].getContentClass().name == this.folderClassName);
-                //     return enabled && items[0].isFolder() && sameClass;
-                // }
-                return false;
+                               
             },
 
             isVisible: function (repository, listType) {
+            	debugger;
                 return this.inherited(arguments);
             },
+
+    		isGlobalEnabled: function(resultSet, items, repository) {		
+    			if (resultSet && resultSet.parentFolder && resultSet.parentFolder.isFolder()) {
+    	    		  var currentFolder =resultSet.parentFolder;
+
+    	    		  if (!currentFolder.hasPrivilege("privAddToFolder"))
+    	    			  return false;
+    	    		 // debugger;
+      			    this.associateEntryTemplate = array.filter(LocalDefinition.getCacheResponce().configurationGridData, function(fld) {
+						if (currentFolder.attributes.FldNumerator && currentFolder.attributes.FldNumerator!=null)
+							return fld.folderClass == currentFolder.getContentClass().id && currentFolder.attributes.FldNumerator.startsWith(fld.orgUnit)
+    							else
+    								return false;
+    						},this);
+
+    				return  this.associateEntryTemplate && this.associateEntryTemplate.length == 1;
+    			}
+    			return false;
+    		},
 
             performAction: function (repository, itemList, callback, teamspace, resultSet, parameterMap) {
                 var destinationFolder = itemList[0];
 
                 var self = this;
 
-                // todo - replace to dynamic configuration
-                var vsId = LocalDefinition.getCacheResponce().configurationGridData[0].searchTemplateVsId;
+               // debugger;
+                this.associateEntryTemplate = array.filter(LocalDefinition.getCacheResponce().configurationGridData, function(fld) {
+					if (destinationFolder.attributes.FldNumerator && destinationFolder.attributes.FldNumerator!=null)
+						return fld.folderClass == destinationFolder.getContentClass().id && destinationFolder.attributes.FldNumerator.startsWith(fld.orgUnit)
+					else
+						return false;
+				},this);
+                if (!(this.associateEntryTemplate && this.associateEntryTemplate.length == 1)){
+                   return;
+                   }
+                debugger;
+                var vsId = this.associateEntryTemplate[0].searchTemplateVsId;
 
                 repository.retrieveSearchTemplate("", vsId, "released", lang.hitch(this, function (searchTemplate) {
                     self.srchDialog = new SearchDialog({
@@ -100,12 +124,12 @@ define(["dojo/_base/declare",
 
             selectTemplate: function (dialog, destinationFolder) {
                 var selectedArr = dialog.search.searchResults.grid.select.row._lastSelectedIds;
-                dialog.destroy();
                 if (!selectedArr) {
-                    console.log("no item selected in this grid");
+                   // console.log("no item selected in this grid");
                     alert("Please select template from the list");
                     return;
                 }
+                dialog.destroy();
                 var documentId = selectedArr[0];
                 Desktop.getDefaultRepository().retrieveItem(documentId, lang.hitch(this, function (currentItem) {
                     this.onDocumentReady(currentItem, destinationFolder);
